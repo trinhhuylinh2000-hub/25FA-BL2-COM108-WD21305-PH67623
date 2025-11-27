f0b4ca89d51b3d8c1e5c8ab4d19d61ea10903cd7
PH67623 <trinhhuylinh2000@gmail.com>
2025-11-27 22:12:48 +0700

    Lab 6 bai 1

---
+++ Lập Trình C/Lập Trình C/Program.c
@@ -0,0 +1,124 @@
+// CConsoleApplication.c : file nay chua 'main' function. 
+// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
+
+#include <stdio.h>
+void kiemTraSoNguyen()
+{
+	printf("Kiem Tra So Nguyen");
+	printf("\n");
+}
+void tinhTrungBinhTongCacSoChiaHetCho3()
+{
+	
+		
+		int n;
+		printf("Nhap so phan tu cua mang: ");
+		scanf("%d", &n);
+
+		
+		int mang[100];
+		int i;
+		for (i = 0; i < n; i++) {
+			printf("Nhap phan tu thu %d: ", i);
+			scanf("%d", &mang[i]);
+		}
+
+		
+		float tong = 0;
+		int count = 0; 
+
+		
+		for (i = 0; i < n; i++) {
+			
+			if (mang[i] % 3 == 0) {
+				tong = tong + mang[i];
+				count++;               
+			}
+		}
+
+		
+		if (count > 0) {
+			float tb = tong / count;
+			printf("Trung binh tong cac so chia het cho 3 la: %.2f\n", tb);
+		}
+		else {
+			printf("Trong mang khong co so nao chia het cho 3.\n");
+		}
+
+		return 0;
+	
+
+}
+
+void lapChucNang(int chonChucNang)
+{
+	int tiepTuc = 1;
+	while (tiepTuc == 1)
+	{
+		switch (chonChucNang)
+		{
+		case 1:
+			kiemTraSoNguyen();
+			break;
+		case 2:
+			tinhTrungBinhTongCacSoChiaHetCho3();
+			break;
+		case 3:
+			// ham goi chuc nang 3
+			break;
+		default:
+			printf("Chon sai. Chuc nang hop le [0-3]");
+			break;
+		}
+
+		printf("Tiep tuc thuc hien chuc nang nay? [1=Co | 0=Khong]: ");
+		scanf("%d", &tiepTuc);
+		system("cls");
+	}
+
+}
+
+int main()
+{
+	int chonChucNang;
+	do
+	{
+		printf("Menu");
+		printf("\n");
+		printf("1. Kiem Tra So Nguyen");
+		printf("\n");
+		printf("2. TEN chuc nang 2");
+		printf("\n");
+		printf("3. TEN chuc nang 3");
+		printf("\n");
+		printf("0. Thoat");
+		printf("\n");
+		printf("Hay chon chuc nang [0-3]: ");
+		scanf("%d", &chonChucNang);
+		lapChucNang(chonChucNang);
+	} while (chonChucNang != 0);
+}
+
+
+
+// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
+
+// Danh cho nguoi moi bat dau:
+//   1. Mo "View" > "Solution Explorer"
+//          de them/quan ly cac files
+//   2. Mo "View" > "Output"
+//          de kiem tra "build output" va "cac thong bao khac"
+//   3. Mo "View" > "Error List"
+//          de xem loi
+//   4. Chon Project > "Add" > "New Item"
+//          de tao moi file code,
+//      hoac Project > "Add" > "Existing Item"
+//          de them file da ton tai vao project nay.
+//   5. Sau nay, de mo lai project nay,
+//          chon "File > Open > Project" va chon file *.sln
+//      Co the xem thu muc project, noi luu file *.sln bang cach
+//          trong cua so "Solution Explorer"
+//          bam chuot phai vao "Solution"
+//          chon "Open Folder in File Explorer"
+
+// GV: AnhTT184
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 25FA-BL2-COM108-WD21305-PH67623
+1.Visual Studio 2022
+2.C Programming
+3.Reterence
+4.Example
